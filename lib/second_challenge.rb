0cc896cad97a309e76ac2d68de33f4c54eabe2c6
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-08 13:23:51 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,18 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      
+        patriarch: {
+          
+        },
+        matriarch: {
+          
+        },
+        hero: {
+          
+        },
+        hero_friend: {
+          
+        }
       }, 
     :capulet => {
       
